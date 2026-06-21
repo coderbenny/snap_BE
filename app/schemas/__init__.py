61eb2c9c156ed2d773395f55212a4c292ed8e18a
coderbenny <codebfullstack@gmail.com>
2026-06-21 18:03:41 +0300

    feat: add sync schemas and Unix ms time helpers

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,9 @@
 from app.schemas.auth_schemas import LoginSchema, LogoutSchema, RefreshSchema, RegisterSchema
 from app.schemas.device_schemas import RegisterDeviceSchema
+from app.schemas.sync_schemas import ClipboardItemSchema, SyncDeleteSchema, SyncPushSchema
 
-__all__ = ['RegisterSchema', 'LoginSchema', 'RefreshSchema', 'LogoutSchema', 'RegisterDeviceSchema']
+__all__ = [
+    'RegisterSchema', 'LoginSchema', 'RefreshSchema', 'LogoutSchema',
+    'RegisterDeviceSchema',
+    'ClipboardItemSchema', 'SyncPushSchema', 'SyncDeleteSchema',
+]
