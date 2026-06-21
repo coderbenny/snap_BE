82b3a978ae734ea766d10b80a67888c86df259c3
coderbenny <codebfullstack@gmail.com>
2026-06-21 18:01:32 +0300

    feat: add ClipboardItem model and Phase 2 migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
+from app.models.clipboard_item import ClipboardItem
 from app.models.device import Device
 from app.models.refresh_token import RefreshToken
 from app.models.user import User
 
-__all__ = ['User', 'Device', 'RefreshToken']
+__all__ = ['User', 'Device', 'RefreshToken', 'ClipboardItem']
